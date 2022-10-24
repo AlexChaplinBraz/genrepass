278ad71e775274a9fd805f66fde91e78293d3a3b
Alexander Chaplin Braz <contact@alexchaplinbraz.com>
2022-10-24 23:55:54 +0200

    Improve benchmark
    Current results:
    
    Parsing words from path           Mean    Samples
    -------------------------------------------------
    load from path: LICENSE      779.23 μs    200/200
    load from path: src/           1.63 ms    199/200
    load from path: examples/      1.43 s     200/200
    
    Single-threaded generation                   Mean        Samples
    ----------------------------------------------------------------
    1 from LICENSE (170 words)                1.31 μs    2,493/2,500
    1 from src/ (2665 words)                  1.25 μs    2,476/2,500
    1 from examples/ (1668079 words)          1.46 μs    2,491/2,500
    10 from LICENSE (170 words)              12.31 μs    2,402/2,500
    10 from src/ (2665 words)                12.52 μs    2,408/2,500
    10 from examples/ (1668079 words)        13.98 μs    2,402/2,500
    100 from LICENSE (170 words)            113.28 μs    2,470/2,500
    100 from src/ (2665 words)              124.85 μs    2,497/2,500
    100 from examples/ (1668079 words)      137.54 μs    2,499/2,500
    1000 from LICENSE (170 words)             1.19 ms    2,500/2,500
    1000 from src/ (2665 words)               1.21 ms    2,498/2,500
    1000 from examples/ (1668079 words)       1.37 ms    2,500/2,500
    10000 from LICENSE (170 words)           12.04 ms    2,499/2,500
    10000 from src/ (2665 words)             12.01 ms    2,499/2,500
    10000 from examples/ (1668079 words)     13.51 ms    2,497/2,500
    
    Multi-threaded generation                    Mean        Samples
    ----------------------------------------------------------------
    1 from LICENSE (170 words)                1.58 μs    2,488/2,500
    1 from src/ (2665 words)                  1.48 μs    2,491/2,500
    1 from examples/ (1668079 words)          1.58 μs    2,479/2,500
    10 from LICENSE (170 words)              17.46 μs    2,468/2,500
    10 from src/ (2665 words)                17.29 μs    2,484/2,500
    10 from examples/ (1668079 words)        15.93 μs    2,482/2,500
    100 from LICENSE (170 words)            106.07 μs    2,483/2,500
    100 from src/ (2665 words)              101.24 μs    2,496/2,500
    100 from examples/ (1668079 words)      104.70 μs    2,482/2,500
    1000 from LICENSE (170 words)           895.90 μs    2,476/2,500
    1000 from src/ (2665 words)             893.40 μs    2,473/2,500
    1000 from examples/ (1668079 words)       1.01 ms    2,468/2,500
    10000 from LICENSE (170 words)            9.40 ms    2,473/2,500
    10000 from src/ (2665 words)             10.04 ms    2,477/2,500
    10000 from examples/ (1668079 words)     10.29 ms    2,497/2,500

--- benches/marks.rs
+++ benches/marks.rs
@@ -1,27 +1,226 @@
-use brunch::{benches, Bench};
+use brunch::{Bench, Benches};
 use genrepass::PasswordSettings;
 use std::time::Duration;
 
 fn main() {
-    let mut ps = PasswordSettings::default();
-    ps.pass_amount = 100;
-    ps.get_words_from_path("LICENSE").unwrap();
+    let mut ps_license = PasswordSettings::default();
+    let mut ps_src = PasswordSettings::default();
+    let mut ps_examples = PasswordSettings::default();
 
-    benches!(inline:
-        Bench::new("single-threaded generation of 100 from LICENSE")
-            .with_timeout(Duration::from_secs(30))
-            .run(|| ps.generate().unwrap()),
-        Bench::new("multi-threaded generation of 100 from LICENSE")
-            .with_timeout(Duration::from_secs(30))
-            .run(|| ps.generate_parallel().unwrap()),
-        Bench::new("single-threaded generation of 100 from src/")
-            .with_timeout(Duration::from_secs(180))
+    println!("Parsing words from path:");
+
+    let mut benches = Benches::default();
+
+    benches.push(
+        Bench::new("load from path: LICENSE")
+            .with_samples(200)
             .run(|| {
-                ps.get_words_from_path("src").unwrap();
-                ps.generate().unwrap();
+                ps_license.clear_words();
+                ps_license.get_words_from_path("LICENSE").unwrap();
             }),
-        Bench::new("multi-threaded generation of 100 from src/")
-            .with_timeout(Duration::from_secs(180))
-            .run(|| ps.generate_parallel().unwrap()),
     );
+    benches.push(
+        Bench::new("load from path: src/")
+            .with_samples(200)
+            .run(|| {
+                ps_src.clear_words();
+                ps_src.get_words_from_path("src").unwrap();
+            }),
+    );
+    benches.push(
+        Bench::new("load from path: examples/")
+            .with_samples(200)
+            .with_timeout(Duration::from_secs(300))
+            .run(|| {
+                ps_examples.clear_words();
+                ps_examples.get_words_from_path("examples").unwrap();
+            }),
+    );
+
+    benches.finish();
+
+    let license_word_len = ps_license.get_words().len();
+    let src_word_len = ps_src.get_words().len();
+    let examples_word_len = ps_examples.get_words().len();
+
+    println!("Single-threaded generation:");
+
+    let mut benches = Benches::default();
+
+    ps_license.pass_amount = 1;
+    ps_src.pass_amount = 1;
+    ps_examples.pass_amount = 1;
+    benches.push(
+        Bench::new(format!("1 from LICENSE ({license_word_len} words)"))
+            .run(|| ps_license.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("1 from src/ ({src_word_len} words)"))
+            .run(|| ps_src.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("1 from examples/ ({examples_word_len} words)"))
+            .run(|| ps_examples.generate().unwrap()),
+    );
+
+    ps_license.pass_amount = 10;
+    ps_src.pass_amount = 10;
+    ps_examples.pass_amount = 10;
+    benches.push(
+        Bench::new(format!("10 from LICENSE ({license_word_len} words)"))
+            .run(|| ps_license.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("10 from src/ ({src_word_len} words)"))
+            .run(|| ps_src.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("10 from examples/ ({examples_word_len} words)"))
+            .run(|| ps_examples.generate().unwrap()),
+    );
+
+    ps_license.pass_amount = 100;
+    ps_src.pass_amount = 100;
+    ps_examples.pass_amount = 100;
+    benches.push(
+        Bench::new(format!("100 from LICENSE ({license_word_len} words)"))
+            .run(|| ps_license.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("100 from src/ ({src_word_len} words)"))
+            .run(|| ps_src.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("100 from examples/ ({examples_word_len} words)"))
+            .run(|| ps_examples.generate().unwrap()),
+    );
+
+    ps_license.pass_amount = 1000;
+    ps_src.pass_amount = 1000;
+    ps_examples.pass_amount = 1000;
+    benches.push(
+        Bench::new(format!("1000 from LICENSE ({license_word_len} words)"))
+            .run(|| ps_license.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("1000 from src/ ({src_word_len} words)"))
+            .run(|| ps_src.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("1000 from examples/ ({examples_word_len} words)"))
+            .run(|| ps_examples.generate().unwrap()),
+    );
+
+    ps_license.pass_amount = 10000;
+    ps_src.pass_amount = 10000;
+    ps_examples.pass_amount = 10000;
+    benches.push(
+        Bench::new(format!("10000 from LICENSE ({license_word_len} words)"))
+            .with_timeout(Duration::from_secs(60))
+            .run(|| ps_license.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("10000 from src/ ({src_word_len} words)"))
+            .with_timeout(Duration::from_secs(60))
+            .run(|| ps_src.generate().unwrap()),
+    );
+    benches.push(
+        Bench::new(format!("10000 from examples/ ({examples_word_len} words)"))
+            .with_timeout(Duration::from_secs(60))
+            .run(|| ps_examples.generate().unwrap()),
+    );
+
+    benches.finish();
+
+    #[cfg(feature = "rayon")]
+    {
+        println!("Multi-threaded generation:");
+
+        let mut benches = Benches::default();
+
+        ps_license.pass_amount = 1;
+        ps_src.pass_amount = 1;
+        ps_examples.pass_amount = 1;
+        benches.push(
+            Bench::new(format!("1 from LICENSE ({license_word_len} words)"))
+                .run(|| ps_license.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("1 from src/ ({src_word_len} words)"))
+                .run(|| ps_src.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("1 from examples/ ({examples_word_len} words)"))
+                .run(|| ps_examples.generate_parallel().unwrap()),
+        );
+
+        ps_license.pass_amount = 10;
+        ps_src.pass_amount = 10;
+        ps_examples.pass_amount = 10;
+        benches.push(
+            Bench::new(format!("10 from LICENSE ({license_word_len} words)"))
+                .run(|| ps_license.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("10 from src/ ({src_word_len} words)"))
+                .run(|| ps_src.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("10 from examples/ ({examples_word_len} words)"))
+                .run(|| ps_examples.generate_parallel().unwrap()),
+        );
+
+        ps_license.pass_amount = 100;
+        ps_src.pass_amount = 100;
+        ps_examples.pass_amount = 100;
+        benches.push(
+            Bench::new(format!("100 from LICENSE ({license_word_len} words)"))
+                .run(|| ps_license.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("100 from src/ ({src_word_len} words)"))
+                .run(|| ps_src.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("100 from examples/ ({examples_word_len} words)"))
+                .run(|| ps_examples.generate_parallel().unwrap()),
+        );
+
+        ps_license.pass_amount = 1000;
+        ps_src.pass_amount = 1000;
+        ps_examples.pass_amount = 1000;
+        benches.push(
+            Bench::new(format!("1000 from LICENSE ({license_word_len} words)"))
+                .run(|| ps_license.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("1000 from src/ ({src_word_len} words)"))
+                .run(|| ps_src.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("1000 from examples/ ({examples_word_len} words)"))
+                .run(|| ps_examples.generate_parallel().unwrap()),
+        );
+
+        ps_license.pass_amount = 10000;
+        ps_src.pass_amount = 10000;
+        ps_examples.pass_amount = 10000;
+        benches.push(
+            Bench::new(format!("10000 from LICENSE ({license_word_len} words)"))
+                .with_timeout(Duration::from_secs(60))
+                .run(|| ps_license.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("10000 from src/ ({src_word_len} words)"))
+                .with_timeout(Duration::from_secs(60))
+                .run(|| ps_src.generate_parallel().unwrap()),
+        );
+        benches.push(
+            Bench::new(format!("10000 from examples/ ({examples_word_len} words)"))
+                .with_timeout(Duration::from_secs(60))
+                .run(|| ps_examples.generate_parallel().unwrap()),
+        );
+
+        benches.finish();
+    }
 }

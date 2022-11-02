fdd8f8c1813736f48197c3bbcb7736b7a2e8263e
Alexander Chaplin Braz <contact@alexchaplinbraz.com>
2022-11-02 22:22:24 +0100

    Update features documentation

--- src/lib.rs
+++ src/lib.rs
@@ -62,7 +62,7 @@ fn run() -> Result<(), Box<dyn Error>> {
 
 # Features
 
-- `serde` — Enables the serialisation and deserialisation of [`PasswordSettings`]
+- `serde` — Enables the serialisation and deserialisation of [`PasswordSettings`] and [`Lexicon`]
 - `rayon` — Enables parallelisation with [`PasswordSettings::generate_parallel()`]
 */
 
